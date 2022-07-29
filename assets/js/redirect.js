4d3c631674aef10b9d53905d43422677342366ce
cooler <52409646+notcooler@users.noreply.github.com>
2022-07-29 14:21:29 +0200

    more redirect stuff

---
+++ assets/js/redirect.js
@@ -0,0 +1,34 @@
+const discord = "";
+const youtube = "https://www.youtube.com/channel/UCRabaDtEYQH0f8l1UlRJyQg";
+const twitter = "https://twitter.com/BusyCooler";
+const github = "https://github.com/notcooler";
+const email = "";
+
+const questionString = new URLSearchParams(window.location.search);
+const redirectValue = questionString.get('rd');
+
+function redirect(url){
+    window.location.assign(url);
+}
+function autoRedirect(){
+    if(redirectValue === "discord"){
+        redirect(discord);
+    }
+    else if(redirectValue === "youtube"){
+        redirect(youtube);
+    }
+    else if(redirectValue === "twitter"){
+        redirect(twitter);
+    }
+    else if(redirectValue === "github"){
+        redirect(github);
+    }
+}
+
+if(redirectValue != null){
+    autoRedirect();
+}
+else{
+    alert("invalid query string");
+    window.close()
+}
